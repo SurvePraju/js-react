05f74a54d83950ef7e51bbc15b92971c4cc5dd47
Prajwal Surve <survepraju99@gmail.com>
2024-02-29 18:23:17 +0530

    prac e-sign

---
+++ src/projects/Esignature/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+
+
+export default function App() {
+    const [name, getName] = useState("");
+    const [date, getDate] = useState("");
+
+    function trackNameChange(event) {
+        const value = event.target.value;
+        getName(value.toUpperCase());
+    }
+    function setDate(event) {
+        const value = event.target.value;
+        getDate(value);
+    }
+    return <div className="container">
+
+
+        <div className="first-container"><div className="y-flex">
+            <h1>{name ? name : "Name"}</h1>
+            <h2>{date ? date : "Date"}</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, reprehenderit in placeat exercitationem sunt praesentium! Placeat  explicabo omnis corporis temporibus accusamus eaquerum qui </p>
+        </div>
+            <div className="x-flex"><input type="text" name="name" placeholder="Name" onChange={trackNameChange} /><input type="date" name="currentDate" id="currentDate" onChange={setDate} /></div>
+        </div>
+
+    </div>
+}
